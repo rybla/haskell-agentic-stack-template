07fd40f355a0b0ede95652b27b04efecf02e7b14
Henry Blanchette <henry@rybl.net>
2026-06-24 14:54:46 -0400

    better test setup

--- test/Test/Lib.hs
+++ test/Test/Lib.hs
@@ -3,25 +3,33 @@
 module Test.Lib (spec) where
 
 import Control.Lens ((^.))
+import Data.Function ((&))
 import Data.Functor ((<&>))
-import Data.List qualified as List
 import Data.Text.Lazy.Encoding qualified as TL
 import Data.Text.Lazy.IO qualified as TLIO
+import Language.Haskell.TH (runIO)
+import Language.Haskell.TH.Syntax qualified as TH
 import Lib (greet)
-import System.FilePath ((</>))
-import System.FilePath.Lens (basename)
+import System.FilePath (joinPath)
+import System.FilePath.Glob (glob)
+import System.FilePath.Lens (basename, directory, (</>~))
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsString)
-import Test.Utilities (listDirectorySuchThat)
 
 spec :: TestTree
-spec = do
+spec =
   testGroup "Lib" . concat $
-    [ $(listDirectorySuchThat ("asset" </> "example") (return . (".input.txt" `List.isSuffixOf`)))
+    [ $( glob (joinPath ["asset", "example", "*.txt"])
+           & runIO
+           >>= TH.lift
+       )
         <&> ( \inputFilepath ->
-                goldenVsString (inputFilepath ^. basename) (inputFilepath <> ".output.txt") $ do
-                  input <- TLIO.readFile inputFilepath
-                  let output = greet input
-                  return . TL.encodeUtf8 $ output
+                goldenVsString
+                  (inputFilepath ^. basename)
+                  (inputFilepath & directory </>~ "output")
+                  $ do
+                    input <- TLIO.readFile inputFilepath
+                    let output = greet input
+                    return . TL.encodeUtf8 $ output
             )
     ]
